f3d073af50253fb60bd402b7b898892e04c9ea55
yagotoasa <1664730993@qq.com>
2020-06-29 20:03:26 +0800

    Triangle function bug fixed

--- app/service/question1.py
+++ app/service/question1.py
@@ -24,8 +24,10 @@ def calendar_atom(arg_list):
 
 def triangle_atom(arg_list):
     a, b, c = arg_list[0], arg_list[1], arg_list[2]
+    if a > 200 or b > 200 or c > 200:
+        return '数值越界'
     if a <= 0 or b <= 0 or c <= 0:
-        return '非三角形'
+        return '数值越界'
     if a + b > c and a + c > b and b + c > a:
         if a == b or a == c or b == c:
             if a == b and b == c:
@@ -33,8 +35,8 @@ def triangle_atom(arg_list):
             elif a * a + b * b == c * c or b * b + c * c == a * a or a * a + c * c == b * b:
                 return '等腰直角三角形'
             else:
-                return '直角三角形'
-        elif a * a + b * b == c * c or b * b + c * c == a * a or a * a + c * c == b * b:
+                return '等腰三角形'
+        if a * a + b * b == c * c or b * b + c * c == a * a or a * a + c * c == b * b:
             return '直角三角形'
         else:
             return '普通三角形'
